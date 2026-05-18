4dc5a498e921391bea8c078d856e0869d50c748b
programmernusantara <pronus.programmernusantara@gmail.com>
2026-05-18 13:50:38 +0700

    first commit

---
+++ terraform/main.tf
@@ -0,0 +1,17 @@
+resource "google_compute_instance" "vm_gitops" {
+  name         = "server-demo-gitops"
+  machine_type = "e2-micro"
+  zone         = "asia-southeast2-a"
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-11"
+    }
+  }
+
+  network_interface {
+    network = "default"
+    access_config {
+    }
+  }
+}
---
+++ terraform/provider.tf
@@ -0,0 +1,21 @@
+terraform {
+  backend "gcs" {
+    bucket  = "belajar-gitops-tfstate-123"
+    prefix  = "terraform/state"
+  }
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "google" {
+  project = var.project_id
+  region  = "asia-southeast2"
+}
+
+variable "project_id" {
+  type = string
+}
